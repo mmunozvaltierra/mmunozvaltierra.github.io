5721d3c6fb0b37cfb5037ee35368b4bbf61d62bc
Mauricio Munoz <mau13-13@hotmail.com>
2024-01-21 21:48:00 -0800

    Create 6 month landing page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { ValentinesPage } from "./pages/ValentinesPage"
+import { Landing } from "./pages/Landing";
 
 export const Routing = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/lovemeplease" element={<ValentinesPage/>} />
+                <Route path="/happy6months" element={<Landing/>} />
             </Routes>
         </Router>
     )
---
+++ src/pages/Landing.js
@@ -0,0 +1,10 @@
+import logo from '../images/happy-cat.gif';
+
+export const Landing = () => {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <img style={{width: '25%'}} src={logo} />
+            <h1>Happy 6 Months!</h1>
+        </div>
+    );
+}
