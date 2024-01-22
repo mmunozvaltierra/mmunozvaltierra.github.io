d2d9265be8bb7b92bdad512ce208704af03485e6
Mauricio Munoz <mau13-13@hotmail.com>
2024-01-21 23:12:10 -0800

    finish initial valentines landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom';
 import logo from '../images/happy-cat.gif';
+import heart from '../images/heart-icon.png';
 
 export const Landing = () => {
     return (
-        <div style={{textAlign: 'center'}}>
+        <div style={{textAlign: 'center', fontFamily:"Hearts"}}>
             <img style={{width: '25%'}} src={logo} />
             <h1>Happy 6 Months!</h1>
+            <Link to="/lovemeplease">
+                <button>
+                    <img src={heart} />
+                </button>
+            </Link>
         </div>
     );
 }
