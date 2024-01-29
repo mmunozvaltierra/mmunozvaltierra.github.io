b3e3606a168797f6914ea7f304076ab3a0d6da17
Mauricio Munoz <mau13-13@hotmail.com>
2024-01-28 19:07:45 -0800

    WIP: set up question page

--- src/pages/ValentinesPage.js
+++ src/pages/ValentinesPage.js
@@ -1,9 +1,17 @@
-
+import cat1 from '../images/flowerCat1.jpg'
+import cat2 from '../images/flowerCat2.jpg'
 
 export const ValentinesPage = () => {
     return (
-        <>
-        <h1>successful test</h1>
-        </>
+        <div style={{textAlign: 'center', fontFamily: 'Hearts'}}>
+            <h1>Will you be my valentine??? </h1>
+            <img src={cat1} style={{maxHeight: '300px'}} />
+            <img src={cat2} style={{maxHeight: '300px'}} />
+            <br />
+            <button>
+            </button>
+            <button>
+            </button>
+        </div>
     );
 }
