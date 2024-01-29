9b8f1d7461dac768c9ecde666a93063690980cf4
Mauricio <mau13-13@hotmail.com>
2024-01-28 22:33:58 -0800

    Add timeout to no page

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,10 @@
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Component } from "react";
 
 import { ValentinesPage } from "./pages/ValentinesPage"
 import { Landing } from "./pages/Landing";
 import { YesPage } from "./pages/YesPage";
+import NoPage from "./pages/NoPage";
 
 export const Routing = () => {
     return (
@@ -11,6 +13,7 @@ export const Routing = () => {
                 <Route path="/lovemeplease" element={<ValentinesPage/>} />
                 <Route path="/happy6months" element={<Landing/>} />
                 <Route path="/yesyesyes" element={<YesPage/>} />
+                <Route path="/doYouMeanIt" element={<NoPage/>} />
             </Routes>
         </Router>
     )
